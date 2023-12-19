531872bf66a8101118303533a62b16980f002846
Chetan Kar <chetankar65@gmail.com>
2023-12-19 16:04:30 +0530

    Made table layout

--- src/components/food/mess/menu.jsx
+++ src/components/food/mess/menu.jsx
@@ -1,7 +1,68 @@
+import "./menu.css"
+
 const Menu = () => {
     return (
-        <div>
-            <h1>Mess - This is the mess component</h1>
+        <div className="mess_menu">
+            <div className="mess_name">
+                <p className="mess_heading"><b>IFC-A</b></p>
+                <table class="menu">
+                    <tr>
+                        <th>Breakfast</th>
+                        <th>Lunch</th>
+                        <th>Dinner</th>
+                    </tr>
+                    <tr>
+                        <td>Poori, aalu curry, break, some shit some shit.Poori, aalu curry, break, some shit some shit</td>
+                        <td>Channa, rice, roti, some shit some shit.Poori, aalu curry, break, some shit some shit</td>
+                        <td>Someshit, someshit, someshit, someshit.Poori, aalu curry, break, some shit some shit</td>
+                    </tr>
+                </table>
+            </div>
+            <div className="mess_name">
+                <p className="mess_heading"><b>IFC-B</b></p>
+                <table class="menu">
+                    <tr>
+                        <th>Breakfast</th>
+                        <th>Lunch</th>
+                        <th>Dinner</th>
+                    </tr>
+                    <tr>
+                        <td>Poori, aalu curry, break, some shit some shit.Poori, aalu curry, break, some shit some shit</td>
+                        <td>Channa, rice, roti, some shit some shit.Poori, aalu curry, break, some shit some shit</td>
+                        <td>Someshit, someshit, someshit, someshit.Poori, aalu curry, break, some shit some shit</td>
+                    </tr>
+                </table>
+            </div>
+            <div className="mess_name">
+                <p className="mess_heading"><b>IFC-C</b></p>
+                <table class="menu">
+                    <tr>
+                        <th>Breakfast</th>
+                        <th>Lunch</th>
+                        <th>Dinner</th>
+                    </tr>
+                    <tr>
+                        <td>Poori, aalu curry, break, some shit some shit.Poori, aalu curry, break, some shit some shit</td>
+                        <td>Channa, rice, roti, some shit some shit.Poori, aalu curry, break, some shit some shit</td>
+                        <td>Someshit, someshit, someshit, someshit.Poori, aalu curry, break, some shit some shit</td>
+                    </tr>
+                </table>
+            </div>
+            <div className="mess_name">
+                <p className="mess_heading"><b>LH</b>&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                <table class="menu">
+                    <tr>
+                        <th>Breakfast</th>
+                        <th>Lunch</th>
+                        <th>Dinner</th>
+                    </tr>
+                    <tr>
+                        <td>Poori, aalu curry, break, some shit some shit.Poori, aalu curry, break, some shit some shit</td>
+                        <td>Channa, rice, roti, some shit some shit.Poori, aalu curry, break, some shit some shit</td>
+                        <td>Someshit, someshit, someshit, someshit.Poori, aalu curry, break, some shit some shit</td>
+                    </tr>
+                </table>
+            </div>
         </div>
     );
 }

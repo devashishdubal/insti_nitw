6f566f24edc75445db1ec12c838c83984c3bb006
shub <shubhampahilwani1@gmail.com>
2023-12-20 09:21:33 +0400

    Style sidebar modify structures

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -5,13 +5,13 @@ const HomeLayout = ({buttonSelect,clickFunction,left,right}) => {
     <div className="full_app">
       <Topbar buttonSelect={buttonSelect} clickFunction={clickFunction} />
       <div className="main">
-        <div className="side">
-            {left}
+          <div className="side">
+              {left}
+          </div>
+          <div className="center">
+            {right}
+          </div>
         </div>
-        <div className="center">
-          {right}
-        </div>
-      </div>
     </div>
   );
 }

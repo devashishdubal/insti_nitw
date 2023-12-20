eec68a1f972614519e3e03cecb92ce1d57427774
Chetan Kar <chetankar65@gmail.com>
2023-12-20 15:37:53 +0530

    Places to eat componenent

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import FoodSidebar from "../components/sidebar/food_sidebar"
 import StudentSidebar from "../components/sidebar/student_sidebar"
 import ClubsSidebar from "../components/sidebar/clubs_sidebar"
 import Menu from "../components/food/mess/menu";
+import Places from "../components/food/places_to_eat/Places";
 
 import "./Home.css"
 
@@ -45,6 +46,8 @@ export default function Home() {
     setSidebarButtonSelect(buttonName);
     if (buttonName == 'mess') {
       setCenterContent(<Menu/>)
+    } else if (buttonName == 'places to eat') {
+      setCenterContent(<Places/>)
     } else {
       setCenterContent(null)
     }

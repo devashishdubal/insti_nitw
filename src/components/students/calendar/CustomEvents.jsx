a715e7c7933fe6a8bfb4b7a7884a1f669fb6d8f1
shub <shubhampahilwani1@gmail.com>
2023-12-26 00:17:39 +0400

    Style custom event

--- src/components/students/calendar/CustomEvents.jsx
+++ src/components/students/calendar/CustomEvents.jsx
@@ -2,24 +2,33 @@ import "./CustomEvents.css";
 
 const CustomEvents = () => {
     return (
-        <div className="custom-wrapper">
-            <div className="custom-desc"> Add Custom Event</div>
-            <div className="label-container">
-                <div className="label-desc">Label:</div>
-                <div className="label-color"></div>
-                <div className="label-color"></div>
-                <div className="label-color"></div>
-                <div className="label-color custom"></div>
-            </div>
-            <div className="title-container">
-                <label for="title" className="title-desc">Title:</label>
-                <input type="text" id="title" name="title" />
-            </div>
-            <div className="datetime-container">
-                <label for="datetime" className="datetime-desc">Date & Time:</label>
-                <input type="time" id="datetime" name="datetime" />
-            </div>
-        </div>
+        <>
+            <form action="https://httpbin.org/post" method="post">
+                <div className="custom-wrapper">
+                    <div className="custom-desc"> Add Custom Event</div>
+                    <div className="label-container custom-wrapper-inner-container">
+                        <div className="desc">Label:</div>
+                        <div className="label-color-container">
+                            <div className="label-color" id="label-color-purple"></div>
+                            <div className="label-color" id="label-color-green"></div>
+                            <div className="label-color" id="label-color-blue"></div>
+                            <input className="label-color" type="color" name="color" />
+                        </div>
+                    </div>
+                    <div className="title-container custom-wrapper-inner-container">
+                        <label for="title" className="desc">Title:</label>
+                        <input type="text" id="title" name="title" />
+                    </div>
+                    <div className="time-submit-container custom-wrapper-inner-container">
+                        <div className="time-container">
+                            <label for="time" className="desc">Time:</label>
+                            <input type="time" id="time" name="time" />
+                        </div>
+                        <button class="submit" type="submit">Add</button>
+                    </div>
+                </div>
+            </form>
+        </ >
     )
 }
 

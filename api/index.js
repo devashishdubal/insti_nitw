37613a81e51a6ed70d4f3bfb1cd5a06adabdc793
Chetan Kar <chetankar65@gmail.com>
2023-12-28 14:40:55 +0530

    Set up backend server

--- api/index.js
+++ api/index.js
@@ -1,1 +1,22 @@
-console.log("IT IS WORKING!!!!!!!!!!! This is a change")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const router = express.Router();
+const path = require("path");
+
+//app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    try {
+        return res.status(200).json("JSON Server is running");
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log("Backend server is running!");
+});

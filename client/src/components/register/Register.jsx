5d3c6c199d296f1be562c1743e1dd36e0cc0374e
devashishdubal <dubaldevashish@gmail.com>
2023-12-30 01:16:43 +0300

    added initial integration for register

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
 import './Register.css'
+import { useState } from 'react';
+import axios from 'axios';
 
 const Register = () => {
+    const [userId,setUserName] = useState(null);
+    const [firstName,setFirstName] = useState(null);
+    const [lastName,setLastName] = useState(null);
+    const [email,setEmail] = useState(null);
+    const [rollNo,setRollNumber] = useState(null);
+    const [password,setPassword] = useState(null);
+    const [confirmPassword,setConfirmPassword] = useState(null);
+
+    const handleRegister = () => {
+        const data = {
+            userId,
+            firstName,
+            lastName,
+            password,
+            email,
+            rollNo
+        };
+        axios.post('http://localhost:8000/api/auth/register',data)
+            .then(() => {
+                console.log("Account added");
+                console.log(data);
+            })
+            .catch((error) => {
+                alert("Error! Please check input fields");
+            });
+        
+    };
     return (
         <div class="register-container">
             <div class="image-bar">
@@ -20,36 +49,36 @@ const Register = () => {
                 <div class="form-container">
                     <div class="form-subtext">Let's do this!</div>
                     <div class="mand">*All fields are mandatory</div>
-                    <form action="./home.html" method="POST">
+                    <form>
                         <div class="form_element">
                             <label for="username">USERNAME</label>
-                            <input type="text" id="username" name="username" required />
+                            <input type="text" id="username" name="username" required onChange={(e) => setUserName(e.target.value)}/>
                         </div>
                         <div class="form_element">
                             <label for="first_name">FIRST NAME</label>
-                            <input type="text" id="first_name" name="first_name" required />
+                            <input type="text" id="first_name" name="first_name" required onChange={(e) => setFirstName(e.target.value)}/>
                         </div>
                         <div class="form_element">
                             <label for="last_name">LAST NAME</label>
-                            <input type="text" id="last_name" name="last_name" required />
+                            <input type="text" id="last_name" name="last_name" required onChange={(e) => setLastName(e.target.value)}/>
                         </div>
                         <div class="form_element">
                             <label for="email">EMAIL</label>
-                            <input type="email" id="email" name="email" required />
+                            <input type="email" id="email" name="email" required onChange={(e) => setEmail(e.target.value)}/>
                         </div>
                         <div class="form_element">
                             <label for="roll">ROLL NUMBER</label>
-                            <input type="text" id="roll" name="roll" required />
+                            <input type="text" id="roll" name="roll" required onChange={(e) => setRollNumber(e.target.value)}/>
                         </div>
                         <div class="form_element">
                             <label for="pass_1">PASSWORD</label>
-                            <input type="password" id="pass_1" name="pass_1" required title="Please use min. 8 letters, including 1 caps, 1 small and 1 special char./number." pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" />
+                            <input type="password" id="pass_1" name="pass_1" required title="Please use min. 8 letters, including 1 caps, 1 small and 1 special char./number." pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" onChange={(e) => setPassword(e.target.value)}/>
                         </div>
                         <div class="form_element">
                             <label for="pass_2">CONFIRM PASSWORD</label>
-                            <input type="password" id="pass_2" name="pass_2" required />
+                            <input type="password" id="pass_2" name="pass_2" required onChange={(e) => setConfirmPassword(e.target.value)}/>
                         </div>
-                        <button type="submit">Create Account</button>
+                        <button type="submit" onClick={handleRegister}>Create Account</button>
                     </form>
                 </div>
                 <div class="instead">Already have an account? <a href="">Login</a></div>

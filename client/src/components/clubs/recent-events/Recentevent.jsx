bd4b3d5549e73c16730d252ba47339930f557c0c
Chetan Kar <chetankar65@gmail.com>
2024-01-04 13:01:32 +0530

    Experimenting with loaders

--- client/src/components/clubs/recent-events/Recentevent.jsx
+++ client/src/components/clubs/recent-events/Recentevent.jsx
@@ -7,11 +7,16 @@ import axios from "axios";
 export default function Recentevent() {
   const [allCards, setAllCards] = useState([]);
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchData = async () => {
         try {
             const response = await axios.get('http://localhost:8000/api/v1/events/recentEvents/');
+            
+            setTimeout(function() {
+              setLoading(false)
+            }, 3000);
             setData(response.data);
 
             const cards = response.data.map((event, index) => ({
@@ -30,6 +35,9 @@ export default function Recentevent() {
   }, []);
   return (
     <>
+    {loading && <div className='loader-content'>
+      <div className='loader'></div></div>}
+    {!loading && (
     <div className='recent-events-container'>
         {allCards.map((card) => (
             <div key={card.id}>
@@ -37,6 +45,8 @@ export default function Recentevent() {
             </div>
         ))}
     </div>
+    )
+    }
     </>
   )
 }

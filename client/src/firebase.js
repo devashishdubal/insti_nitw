f924c25b0bc43369798acd00d82e7b868cda673d
Chetan Kar <chetankar65@gmail.com>
2024-01-05 11:49:45 +0530

    Environment variables

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -10,13 +10,13 @@ import 'firebase/compat/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB_GHE9wLbkf8Agl39N9yE2y4cnmzDVlwk",
-  authDomain: "nitw-nexus.firebaseapp.com",
-  projectId: "nitw-nexus",
-  storageBucket: "nitw-nexus.appspot.com",
-  messagingSenderId: "1016121703230",
-  appId: "1:1016121703230:web:0123296cb398b2f40a8a6f",
-  measurementId: "G-NP5DJYK11Q"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase

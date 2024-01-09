a3ee313c030c687afba81f0bb19e21229d84f0fc
shub <shubhampahilwani1@gmail.com>
2024-01-09 23:50:33 +0530

    Minor change in forum to fix the hover effects

--- client/src/components/forum/questions/question_card.jsx
+++ client/src/components/forum/questions/question_card.jsx
@@ -7,7 +7,7 @@ import { AuthContext } from "../../../Context/AuthContext"
 const QuestionCard = ({ comments, fetch, id, user, date, title, description, tags, index, likes, dislikes, liked, disliked }) => {
     const { currentUser, userDetails } = useContext(AuthContext)
     const [likeColor, setLikeColor] = useState(liked ? "lightgreen" : "white");
-    const [dislikeColor, setDislikeColor] = useState(disliked ? "lightcoral": "white")
+    const [dislikeColor, setDislikeColor] = useState(disliked ? "lightcoral" : "white")
 
     const updateLike = () => {
         axios
@@ -37,7 +37,7 @@ const QuestionCard = ({ comments, fetch, id, user, date, title, description, tag
             .then(() => {
                 fetch();
                 disliked = !disliked;
-                
+
                 if (disliked) {
                     setDislikeColor("lightcoral")
                     if (liked) {
@@ -84,7 +84,7 @@ const QuestionCard = ({ comments, fetch, id, user, date, title, description, tag
             .then(() => {
                 fetch();
                 disliked = !disliked;
-                
+
                 if (disliked) {
                     setDislikeColor("lightcoral")
                     if (liked) {
@@ -105,9 +105,10 @@ const QuestionCard = ({ comments, fetch, id, user, date, title, description, tag
 
     return (
         <>
-            <Link to={`/profile/${user}`}>
-                <div className="username" ><i>u/{user}</i> • {date}</div>
-            </Link>
+            <div className="username">
+                <Link to={`/profile/${user}`}>
+                    <i>u/{user}</i> </Link>• {date}
+            </div>
             <Link to={`/students/forum/${id}`}>
                 <div className="content">
                     <p className="text">{title}</p>
@@ -122,11 +123,11 @@ const QuestionCard = ({ comments, fetch, id, user, date, title, description, tag
                                 {comments}
                             </button>
                             <button className="like_button feedback">
-                                <svg style={{fill: likeColor}} onClick={handleLikeCick} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path className="avoid" d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path></svg>
+                                <svg style={{ fill: likeColor }} onClick={handleLikeCick} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path className="avoid" d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path></svg>
                                 {likes}
                             </button>
                             <button className="dislike_button feedback">
-                                <svg style={{fill: dislikeColor}} onClick={handleDislikeCick} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path className="avoid" d="M10 15v4a3 3 0 0 0 3 3l4-9V2H5.72a2 2 0 0 0-2 1.7l-1.38 9a2 2 0 0 0 2 2.3zm7-13h2.67A2.31 2.31 0 0 1 22 4v7a2.31 2.31 0 0 1-2.33 2H17"></path></svg>
+                                <svg style={{ fill: dislikeColor }} onClick={handleDislikeCick} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path className="avoid" d="M10 15v4a3 3 0 0 0 3 3l4-9V2H5.72a2 2 0 0 0-2 1.7l-1.38 9a2 2 0 0 0 2 2.3zm7-13h2.67A2.31 2.31 0 0 1 22 4v7a2.31 2.31 0 0 1-2.33 2H17"></path></svg>
                                 {dislikes}
                             </button>
                         </div>

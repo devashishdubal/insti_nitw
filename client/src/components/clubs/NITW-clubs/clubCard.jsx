b80732c7ba766b9a9fc366a61d54d2004efea3f3
shub <shubhampahilwani1@gmail.com>
2024-01-10 21:45:13 +0530

    Tried to remove client npm start warnings but too much

--- client/src/components/clubs/NITW-clubs/clubCard.jsx
+++ client/src/components/clubs/NITW-clubs/clubCard.jsx
@@ -1,15 +1,15 @@
 import "./clubs.css"
-import React, {useContext, useState, useEffect} from "react";
+import React, { useContext, useState } from "react";
 import axios from "axios";
 import { AuthContext } from "../../../Context/AuthContext";
 
-const ClubCard = ({imageLink, clubName, clubDescription, clubId, isSubscribed}) => {
-    const { currentUser, userDetails } = useContext(AuthContext)
+const ClubCard = ({ imageLink, clubName, clubDescription, clubId, isSubscribed }) => {
+    const { userDetails } = useContext(AuthContext)
     const [subscribeStatus, setSubscribeStatus] = useState(isSubscribed);
     const subscriptionHandler = async (e) => {
         e.preventDefault();
         axios
-            .put("http://localhost:8000/api/v1/clubs/handleSubscribe", {"clubId": clubId, "username": userDetails.username})
+            .put("http://localhost:8000/api/v1/clubs/handleSubscribe", { "clubId": clubId, "username": userDetails.username })
             .then(() => {
                 //fetch();
                 //isSubscribed = !isSubscribed;
@@ -23,7 +23,7 @@ const ClubCard = ({imageLink, clubName, clubDescription, clubId, isSubscribed}) 
     const unsubscriptionHandler = async (e) => {
         e.preventDefault();
         axios
-            .put("http://localhost:8000/api/v1/clubs/handleUnsubscribe", {"clubId": clubId, "username": userDetails.username})
+            .put("http://localhost:8000/api/v1/clubs/handleUnsubscribe", { "clubId": clubId, "username": userDetails.username })
             .then(() => {
                 //fetch();
                 setSubscribeStatus(!subscribeStatus)
@@ -32,26 +32,26 @@ const ClubCard = ({imageLink, clubName, clubDescription, clubId, isSubscribed}) 
                 console.log(error);
             });
     }
-    
+
     return (
-    <div className="club_card">
-        <div className="logo">
-            <img src={imageLink} alt="logo" className="logo-img"/>
+        <div className="club_card">
+            <div className="logo">
+                <img src={imageLink} alt="logo" className="logo-img" />
+            </div>
+            <span className="club_name"><p>{clubName}</p></span>
+            <div className="info">
+                {clubDescription}
+            </div>
+            {
+                subscribeStatus ? (
+                    <button className="subscribe_button" onClick={unsubscriptionHandler}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>                Subscribed
+                    </button>) :
+                    (<button className="subscribe_button" onClick={subscriptionHandler}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 17H2a3 3 0 0 0 3-3V9a7 7 0 0 1 14 0v5a3 3 0 0 0 3 3zm-8.27 4a2 2 0 0 1-3.46 0"></path></svg>            Subscribe
+                    </button>)
+            }
         </div>
-        <span className="club_name"><p>{clubName}</p></span>
-        <div className="info">
-            {clubDescription}
-        </div>
-        {
-            subscribeStatus ? (
-            <button className="subscribe_button" onClick={unsubscriptionHandler}>
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>                Subscribed
-            </button>) :
-            (<button className="subscribe_button" onClick={subscriptionHandler}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 17H2a3 3 0 0 0 3-3V9a7 7 0 0 1 14 0v5a3 3 0 0 0 3 3zm-8.27 4a2 2 0 0 1-3.46 0"></path></svg>            Subscribe
-            </button>)
-        }
-    </div>
     );
 }
 
--- client/src/components/forum/answers/answer_card.jsx
+++ client/src/components/forum/answers/answer_card.jsx
@@ -2,18 +2,18 @@ import "./answers.css"
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../Context/AuthContext"
-import React, {useState, useContext, useEffect} from 'react'
+import React, { useState, useContext } from 'react'
 
 const AnswerCard = ({ id, answer, fetch, questionId, userHasLiked, userHasDisliked }) => {
-    const { currentUser, userDetails } = useContext(AuthContext)
+    const { userDetails } = useContext(AuthContext)
     const [likeColor, setLikeColor] = useState(userHasLiked ? "lightgreen" : "white");
-    const [dislikeColor, setDislikeColor] = useState(userHasDisliked ? "lightcoral": "white")
+    const [dislikeColor, setDislikeColor] = useState(userHasDisliked ? "lightcoral" : "white")
     const updateLike = () => {
         axios
             .put(`http://localhost:8000/api/v1/forum/updateLikes/comments/${id}?userId=${userDetails.username}&questionId=${questionId}`)
             .then(() => {
                 fetch();
-        
+
             })
             .catch((error) => {
                 console.log(error);
@@ -64,7 +64,7 @@ const AnswerCard = ({ id, answer, fetch, questionId, userHasLiked, userHasDislik
             .then(() => {
                 fetch();
                 userHasDisliked = !userHasDisliked;
-                
+
                 if (userHasDisliked) {
                     setDislikeColor("lightcoral")
                     if (userHasLiked) {
@@ -92,11 +92,11 @@ const AnswerCard = ({ id, answer, fetch, questionId, userHasLiked, userHasDislik
                 <p className="description">{answer.answerDescription}</p>
                 <div className="likes">
                     <button className="like_button">
-                        <svg style={{fill: likeColor}} onClick={handleLikeCick} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path></svg>
+                        <svg style={{ fill: likeColor }} onClick={handleLikeCick} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path></svg>
                         {answer.likes}
                     </button>
                     <button className="dislike_button">
-                        <svg style={{fill: dislikeColor}} onClick={handleDislikeClick}  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10 15v4a3 3 0 0 0 3 3l4-9V2H5.72a2 2 0 0 0-2 1.7l-1.38 9a2 2 0 0 0 2 2.3zm7-13h2.67A2.31 2.31 0 0 1 22 4v7a2.31 2.31 0 0 1-2.33 2H17"></path></svg>
+                        <svg style={{ fill: dislikeColor }} onClick={handleDislikeClick} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10 15v4a3 3 0 0 0 3 3l4-9V2H5.72a2 2 0 0 0-2 1.7l-1.38 9a2 2 0 0 0 2 2.3zm7-13h2.67A2.31 2.31 0 0 1 22 4v7a2.31 2.31 0 0 1-2.33 2H17"></path></svg>
                         {answer.dislikes}
                     </button>
                 </div>

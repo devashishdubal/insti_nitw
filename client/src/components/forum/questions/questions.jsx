3d7b589891a416f6635d53494b03a9fe2b35aff7
shub <shubhampahilwani1@gmail.com>
2024-01-20 22:36:17 +0530

    Remove some console errors

--- client/src/components/forum/questions/questions.jsx
+++ client/src/components/forum/questions/questions.jsx
@@ -97,7 +97,7 @@ const Questions = () => {
                 </div>
                 <div className='intro_right'>
                     <button className={`search_place ${isSearchFocused ? 'focused' : ''}`}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
                         <input
                             type="text"
                             placeholder='Search'

b063de70fc8a9e3380d0f266ea69555b6063fe20
devashishdubal <dubaldevashish@gmail.com>
2024-01-24 23:53:10 +0530

    added some integration code

--- client/src/components/clubs/edit-event/EventCard.jsx
+++ client/src/components/clubs/edit-event/EventCard.jsx
@@ -7,8 +7,23 @@ const EventCard = () => {
   const [club,setClub] = useState('CSES');
   const fetchClubEvents = async () => {
     try{
-      const response = await axios.get(`http://localhost:8000/api/v1/events/getEventDetails/${club}`)
-      setEvents(response.data);
+      const response = await axios.get(`http://localhost:8000/api/v1/events/getEventDetails/6592a8cd1f5a26e6d44749c6`)
+      const e1 = response.data.map((event,index) => ({
+        id: index+1,
+        card:
+          <Link to={`/clubs/edit_event/${event._id}`}>
+            <div className="event-card">
+                {/* <img src={imageUrl} alt={title} className="event-image" /> */}
+                <div className="event-details">
+                    <h3 className="event-title">{event._doc.eventName}</h3>
+                    <p className="event-info">{event._doc.eventDateTime}</p>
+                    {/* <p className="event-info">Time</p> */}
+                    <p className="event-info">{event._doc.eventVenue}</p>
+                </div>
+              </div>
+          </Link>
+      }));
+      setEvents(e1);
     }
     catch(e) {
       console.log(e);
@@ -21,17 +36,15 @@ const EventCard = () => {
   
   return (
     <>
-        <Link to={`/clubs/edit_event/1`}>
-        <div className="event-card">
-            {/* <img src={imageUrl} alt={title} className="event-image" /> */}
-            <div className="event-details">
-                <h3 className="event-title">Title</h3>
-                <p className="event-info">Date</p>
-                <p className="event-info">Time</p>
-                <p className="event-info">Venue</p>
+        {events.length === 0 ? (
+          <p>Loading...</p>
+        ):(
+          events.map((event) => {
+            <div key={event.id}>
+              {event.card}
             </div>
-            </div>
-        </Link>
+        }
+        ))}
     </>
   )
 }
